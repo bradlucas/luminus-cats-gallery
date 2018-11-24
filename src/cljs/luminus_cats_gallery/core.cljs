96324ec964b9349158543f75aacebabebeb15c1e
Brad Lucas <brad@beaconhill.com>
2018-11-23 21:21:50 -0500

    adding pagination

--- src/cljs/luminus_cats_gallery/core.cljs
+++ src/cljs/luminus_cats_gallery/core.cljs
@@ -49,21 +49,74 @@
 ;;       [:div {:dangerouslySetInnerHTML
 ;;              {:__html (md->html docs)}}]])])
 
+;; Version 1 - Show list of Cats
+;; (defn fetch-links! [links cnt] 
+;;   (GET "/api/cats" {:params {:cnt cnt}
+;;                     :handler #(reset! links %)}))
+;;
+;; (defn home-page [] 
+;;   (let [links (r/atom nil)] 
+;;     (fetch-links! links 20) 
+;;     (fn []
+;;       (if (not-empty @links)
+;;         [:div
+;;          [:h2 "Cats"]
+;;          [:ul {:class "list-group"}]
+;;          (for [link @links]
+;;            [:li {:class "list-group-item"} [:img {:src link}]])]
+;;         [:div "Standby for cats!"]))))
+
+;; Version 2 - Pagenation
 (defn fetch-links! [links cnt] 
+  ;; partition links into groups of six
   (GET "/api/cats" {:params {:cnt cnt}
-                    :handler #(reset! links %)}))
+                    :handler #(reset! links (vec (partition-all 6 %)))}))
+
+(defn images [links]
+  [:div.text-xs-center
+   (for [row (partition-all 3 links)]
+     ^{:key row}
+     [:div.row
+      (for [link row]
+        ^{:key link}
+        [:div.col-sm-4 [:img {:width 400 :src link}]])])])
+
+(defn forward [i pages]
+  (if (< i (dec pages)) (inc i) i))
+
+(defn back [i]
+  (if (pos? i) (dec i) i))
+
+(defn page-nav-link [page i]
+  [:li.page.item>a.page-link.btn.btn-primary
+   {:on-click #(reset! page i)
+    :class (when (= i @page) "active")}
+   [:span i]])
+
+(defn pager [pages page]
+  (when (> pages 1)
+    (into
+     [:div.text-xs-center>ul.pagination.pagination-lg]
+     (concat
+      [[:li.page-item>a.page-link.btn
+        {:on-click #(swap! page back pages)
+         :class (when (= @page 0) "disabled")}
+        [:span "<<"]]]
+      (map (partial page-nav-link page) (range pages))
+      [[:li.page-item>a.page-link.btn
+        {:on-click #(swap! page forward pages)
+         :class (when (= @page (dec pages)) "disabled")}
+        [:span ">>"]]]))))
 
 (defn home-page [] 
-  (let [links (r/atom nil)] 
-    (fetch-links! links 20) 
+  (let [links (r/atom nil)
+        page (r/atom 0)] 
+    (fetch-links! links 50) 
     (fn []
-      (if (not-empty @links)
-        [:div
-         [:h2 "Cats"]
-         [:ul {:class "list-group"}]
-         (for [link @links]
-           [:li {:class "list-group-item"} [:img {:src link}]])]
-        [:div "Standby for cats!"]))))
+      (when @links
+        [:div.container->div.row>div.col-md-12
+         [pager (count @links) page]
+         [images (@links @page)]]))))
 
 (def pages
   {:home #'home-page
